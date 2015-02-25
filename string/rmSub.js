f24912f593acd5f9a31bd769344dac2c60541684
qubick <qubick.kim@gmail.com>
2015-02-25 10:53:51 -0700

    simple solution

--- string/rmSub.js
+++ string/rmSub.js
@@ -1,5 +1,5 @@
 function remove_substring(s, p, n){
-
+/*
 	var str = s.split('')
 	for (var j=0; j<n; j++){
 		for (var i=p; i<s.length; i++){
@@ -8,6 +8,9 @@ function remove_substring(s, p, n){
 		str[s.length-i-j] = ''
 	}
 	console.log(str.join(""))
+	*/
+
+	console.log(s.substring(0,p-1)+s.substring(p+n-1,s.length))
 }
 
 s = "abcdefghi"
