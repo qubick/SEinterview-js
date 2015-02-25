722d1b5e7b2507941d3953e418b541cf77094542
qubick <qubick.kim@gmail.com>
2015-02-24 23:55:46 -0700

    basic hash

---
+++ hash/hash.js
@@ -0,0 +1,16 @@
+function hash(){
+
+}
+
+function main(){
+	var hash = {}
+
+	hash['key1'] = "value1"
+	hash['key2'] = "value2"
+
+	var search = 'key1'
+
+	return console.log(hash[search])
+}
+
+main()
