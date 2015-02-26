dc604e21a8bb227d8adffce78fb50bcb78269eb1
qubick <qubick.kim@gmail.com>
2015-02-25 19:55:03 -0700

    find missing number from given 1-N numbers

---
+++ hash/missingNo.js
@@ -0,0 +1,20 @@
+function find_missing_number(v){
+
+	var max = 0
+	var hash = {}
+
+	for(var i=0; i<v.length; i++){
+		if(v[i]> max){
+			max = v[i]
+			//console.log(max)
+		}
+		hash[v[i]] = true
+	}
+
+	for(var j=1; j<max; j++)
+		if(hash[j] != true)
+		 	console.log(j)
+}
+
+v = [5, 4, 1, 6]
+find_missing_number(v);
