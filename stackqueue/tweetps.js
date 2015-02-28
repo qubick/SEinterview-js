a96984f7b79ebf961d14c81605a3490f8a1ac929
qubick <qubick.kim@gmail.com>
2015-02-28 15:09:45 -0700

    test with 3 seconds

--- stackqueue/tweetps.js
+++ stackqueue/tweetps.js
@@ -2,21 +2,20 @@ function tweets_per_second(tps, k) {
     // Write your code here
     // To print results to the standard output please use console.log()
     // Example: console.log("Hello world!");
-    if(tps.length == 1)
-        console.log(tps[0])
-    else if (tps.length ==2)
-        console.log((tps[0]>tps[1])?tps[0]:tps[1])
-    else{  
-        var queue = new Queue()
-        for(var i=0; i<tps.length; i++){
-            queue.push(new Node(tps[i]))
+    
+    var queue = new Queue()
+    for(var i=0; i<tps.length; i++){
+        queue.push(new Node(tps[i]))
+    }
+    
+    for(var j=0; j<tps.length-k; j++){
+        var arr = queue.pop(k)
+        var max = 0
+        for(var k=0; j<arr.length; j++){
+            if(arr[k]>max)
+                max = arr[k]
         }
-        
-        for(var j=0; j<tps.length-3; i++){
-            var arr = queue.pop();
-            if(arr[0]>arr[1]?var max=arr[0]:var max=arr[1])
-                if(max>arr[2]?console.log(max):console.log(arr[2])
-        }
+        console.log(max)
     }
 }
 
@@ -24,9 +23,13 @@ function Queue(){
     this.length = 0
     this.head = null
     this.tail = null
-    
+
     this.push = function(node){
-        if(this.head === this.tail){
+     
+        if(!this.head){
+            this.head = node
+            this.tail = node
+        } else if(this.head === this.tail){
             this.head.next = node
             this.tail = node
         } else {
@@ -35,9 +38,16 @@ function Queue(){
         }
     }
     
-    this.pop = function(){
-        var res = [this.head.data, this.head.next.data, this.head.next.data]
-        this.head = this.head.next
+    this.pop = function(num){
+        var res = []
+        var temp = this.head
+        for(var i=0; i<num; i++){
+            res[i] = temp.data
+            if(temp.next)
+                temp = temp.next
+            else
+                break
+        } 
         return res
     }
     
@@ -60,3 +70,7 @@ function Node(value){
     this.data = value
     this.next = null
 }
+
+var tps = [6, 9, 4, 7, 4, 1]
+k = 3
+tweets_per_second(tps, k)
