46c40b036857a064e8e100bfc39198aff6702535
qubick <qubick.kim@gmail.com>
2015-02-28 15:38:09 -0700

    large window size should return correct results as well

--- stackqueue/tweetps.js
+++ stackqueue/tweetps.js
@@ -4,16 +4,40 @@ function tweets_per_second(tps, k) {
     // Example: console.log("Hello world!");
     
     var queue = new Queue()
+
+    var log = 0
+    
     for(var i=0; i<tps.length; i++){
         queue.push(new Node(tps[i]))
     }
     
-    for(var j=0; j<tps.length-k; j++){
+    for(var j=1; j<=k; j++){
+        var arr = queue.pop(j)
+        var max = 0
+        for(var l=0; l<arr.length; l++){
+            if(arr[l]>max)
+                max = arr[l]
+        }
+        console.log(max)
+    }
+    for(var j=k+1; j<=tps.length-k; j++){
         var arr = queue.pop(k)
         var max = 0
-        for(var k=0; j<arr.length; j++){
-            if(arr[k]>max)
-                max = arr[k]
+        for(var l=0; l<arr.length; l++){
+            if(arr[l]>max){
+                max = arr[l]
+            }
+        }
+        console.log(max)
+    }
+    for(var j=k; j>0; j--){
+        //move head to back to take node from later part of the queue
+        var arr = queue.pop(j)
+        var max = 0
+        for(var l=0; l<arr.length; l++){
+            if(arr[l]>max){
+                max = arr[l]
+            }
         }
         console.log(max)
     }
@@ -41,11 +65,12 @@ function Queue(){
     this.pop = function(num){
         var res = []
         var temp = this.head
+        
         for(var i=0; i<num; i++){
             res[i] = temp.data
-            if(temp.next)
+            if(temp.next){
                 temp = temp.next
-            else
+            }else
                 break
         } 
         return res
@@ -70,7 +95,8 @@ function Node(value){
     this.data = value
     this.next = null
 }
-
+/*
 var tps = [6, 9, 4, 7, 4, 1]
 k = 3
 tweets_per_second(tps, k)
+*/
