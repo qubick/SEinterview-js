74a9b3108f149ed3331fdb795ceb3c4d2d95b76a
qubick <qubick.kim@gmail.com>
2015-03-02 12:43:26 -0700

    regex basic practice

---
+++ regex/examples.js
@@ -0,0 +1,9 @@
+function sample_practice(str){
+	var res = str.replace(/[Uu]niversity/, "highschool")
+
+	var ex = 13
+	return console.log(~~ex)
+}
+
+str = "Visit Hniversity of Colorado!"
+sample_practice(str)
