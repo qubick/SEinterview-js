8e2e31575eba3c26153436d02475cfddffe74151
qubick <qubick.kim@gmail.com>
2015-03-02 21:45:12 -0700

    find all vowels regardless of case and count

---
+++ regex/vowelCnt.js
@@ -0,0 +1,18 @@
+function count_vowels(s){
+
+/*
+	var cnt = 0
+	for(var i=0; i<s.length; i++){
+		if(s[i].match(/[aeiou]/i))
+			//console.log("white space found")
+			cnt++
+	}
+	return console.log(cnt)
+*/
+	console.log(s.match(/[aeiou]/ig).length)
+	// flag i for ignore case
+	// flag g for find all occurence
+}
+
+s = "This Is sunday"
+count_vowels(s)
