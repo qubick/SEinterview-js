8339ea2402b16c4f81f97b46a091c655b3a4feb3
qubick <qubick.kim@gmail.com>
2015-03-04 19:08:25 -0700

    cannot verified for really long input.. where do I have wrong answer?

--- stackqueue/tweetps.js
+++ stackqueue/tweetps.js
@@ -1,34 +1,29 @@
 function tweets_per_second(tps, k) {
-    // Write your code here
-    // To print results to the standard output please use console.log()
-    // Example: console.log("Hello world!");
     
     var queue = new Queue()
-    var log = 0
     
     for(var i=0; i<=tps.length-k; i++){
-        queue.push(new Node(tps[i]))
-        var res = queue.pop(i+1, k)
-        console.log(res.length)
+        queue.push(new Node(tps[i]), k)
+        
+		  var res = queue.pop(i+1)
         var max = 0
         
         for(var j=0; j<res.length; j++){
             if(max < res[j])
                 max = res[j]
-            console.log(res)
         }
-        //console.log(max)
+        console.log(max)
     }
     for(var i=tps.length-k+1; i<tps.length; i++){
-        queue.push(new Node(tps[i]))
-        var res = queue.pop(i, k)
+        queue.push(new Node(tps[i]),k)
+        var res = queue.pop(i+1)
         var max = 0
         
         for(var j=0; j<res.length; j++){
             if(max < res[j])
                 max = res[j]
         }
-        //console.log(max)
+        console.log(max)
     }
 }
 
@@ -37,22 +32,28 @@ function Queue(){
     this.head = null
     this.tail = null
 
-    this.push = function(node){
+    this.push = function(node,k){
      
         if(!this.head){
             this.head = node
             this.tail = node
+        this.length++
         } else if(this.head === this.tail){
             this.head.next = node
             this.tail = node
-        } else {
+        this.length++
+        } else if(this.length < k){
             this.tail.next = node
             this.tail = node
-        }
         this.length++
+        } else {
+		 		this.tail.next = node
+				this.tail = node
+				this.head = this.head.next
+		  }
     }
     
-    this.pop = function(num, k){
+    this.pop = function(num){
         var res = []
         var temp = this.head
         
@@ -60,13 +61,7 @@ function Queue(){
             res[i] = temp.data
             if(temp.next){
                 temp = temp.next
-                if(this.length > k)
-                    //console.log(this.length, k)
-                    this.head = this.head.next
             }else{
-                if(this.length > k)
-                    //console.log(this.length, k)
-                    this.head = this.head.next
                 break
             }
         } 
