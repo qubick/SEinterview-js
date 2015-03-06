5837b5e0e5e04e3fda060a04d6b951e8e5b4f13d
qubick <qubick.kim@gmail.com>
2015-03-05 23:45:23 -0700

    bubble sort

--- sorting/basicBubble.js
+++ sorting/basicBubble.js
@@ -2,14 +2,15 @@ function sort_students(numbers) {
     
 	 //var changed = false
 	 for(var i=0; i<numbers.length; i++){
-	 	for(var j=1; i<numbers.length-1; j++){
-			//changed = false
-            if(numbers[j-1] > numbers[j]){// && !changed){
+	 console.log("loop started")
+	 	for(var j=1; i<numbers.length; j++){
+            
+				if(numbers[j-1] > numbers[j]){// && !changed){
                 var temp = numbers[j]
                 numbers[j] = numbers[j-1]
                 numbers[j-1] = temp
 					//changed = true
-					break
+					console.log(numbers[j-1], numbers[j])
             }
         }
     }
@@ -17,4 +18,5 @@ function sort_students(numbers) {
 }
 
 numbers = [5,3,5,2]
+number = [1,9,4,5,7]
 sort_students(numbers)
