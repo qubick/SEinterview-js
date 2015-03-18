13c39de5567b6bc228cf1f4507abc544a5f80996
qubick <qubick.kim@gmail.com>
2015-03-18 15:52:00 -0600

    add neighbors

--- graph/buildLikedGrph.js
+++ graph/buildLikedGrph.js
@@ -3,8 +3,16 @@ function Node(value){
 	this.neighbor = []
 }
 
-function Graph(node){
-	this.head = node
+function Graph(){
+	this.addNabor = function(node, nabor){
+		if(nabor instanceof(Array)){
+			nabor.forEach(function(n){
+				node.neighbor.push(n)
+			});
+		} else {
+			node.neighbor.push(nabor)
+		}
+	}
 }
 
 function cost(x,y){ //only returns direct cost
@@ -17,16 +25,23 @@ function cost(x,y){ //only returns direct cost
 
 function main(){
 
-	var head = new Node(10)
-		,B = new Node(1)
-		,C = new Node(2)
-	
-	var graph = new Graph(head);
-	hade.next = B;
-	B.next = C;
-	//C.next = null
+	var node1 = new Node(1)
+		,node2 = new Node(2)
+		,node3 = new Node(3)
+		,node4 = new Node(4)
+		,node5 = new Node(5)
+		,node6 = new Node(6)
 
-	
+	var graph = new Graph();
+
+	graph.addNabor(node1, [node2, node3])
+	graph.addNabor(node2, [node1, node4])
+	graph.addNabor(node3, [node1, node4, node5, node6])
+	graph.addNabor(node4, [node2, node3, node5])
+	graph.addNabor(node5, [node3, node4, node6])
+	graph.addNabor(node6, [node3, node5])
+
+	//console.log(node3.neighbor.data)
 }
 
 main()
