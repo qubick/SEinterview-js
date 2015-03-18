8ba745d78ca8ccd16febc9ee8850fece78d7afa7
qubick <qubick.kim@gmail.com>
2015-03-18 16:24:52 -0600

    depth first search

--- graph/buildLikedGrph.js
+++ graph/buildLikedGrph.js
@@ -1,9 +1,12 @@
 function Node(value){
 	this.data = value
 	this.neighbor = []
+	this.visited = false
 }
 
 function Graph(){
+	var stack = new Stack()
+	
 	this.addNabor = function(node, nabor){
 		if(nabor instanceof(Array)){
 			nabor.forEach(function(n){
@@ -11,6 +14,53 @@ function Graph(){
 			});
 		} else {
 			node.neighbor.push(nabor)
+		}
+	}
+
+	this.dfs = function(node){
+
+		for(var i=0; i<node.neighbor.length; i++){
+			if(node.neighbor[i].visited === false)
+				stack.push(node.neighbor[i])
+		}
+
+		if(node.visited === false){
+			node.visited = true
+			console.log(node.data)
+		}
+
+		while(!stack.empty){
+			var next = stack.pop()
+			if(next)
+				this.dfs(next)
+			else
+				break
+		}
+	}
+}
+
+function Stack(){
+	this.data = []
+	this.empty = true
+	this.full = false
+	this.topP = 0
+
+	this.push = function(node){
+		if(!this.full){
+			this.data[this.topP++] = node
+			this.empty = false
+		}else{
+			console.log("stack is full")
+			this.full = true
+		}
+	}
+
+	this.pop = function(){
+		if(!this.empty){
+			return this.data[--this.topP]
+		} else {
+			console.log("stack is empty")
+			this.empty = true
 		}
 	}
 }
@@ -41,7 +91,8 @@ function main(){
 	graph.addNabor(node5, [node3, node4, node6])
 	graph.addNabor(node6, [node3, node5])
 
-	//console.log(node3.neighbor.data)
+	//console.log(cost(node1,node5))
+	graph.dfs(node2)
 }
 
 main()
