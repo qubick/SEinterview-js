69aa51b3e757cf9b126c672f1224d215f44ff012
qubick <qubick.kim@gmail.com>
2017-02-12 11:06:47 -0700

    count the number of unique pairs

--- hash/pairSum.js
+++ hash/pairSum.js
@@ -1,19 +1,54 @@
-function pairSum(v, sum){
-	
+// find the number of pairs in the array
+// the pair that makes given sum
+
+function pairSum(v, sum){ //return true or false
+
 	var index = []
+			,cnt = 0
 
 	//o(n)
 	for(var i=0; i<v.length; i++){
 		if(index[sum-v[i]] !== undefined){
 			console.log(1)
 			return
-		} else
+		}
+		else {
 			index[v[i]] = true
+		}
 	}
 	console.log(0)
 }
 
-v = [2,2,20,18]
+
+function pair(v, sum){
+  var newArr = []
+			,hash = [], hash1 = []
+      ,cnt = 0
+
+	for(var x=0; x<v.length; x++)
+		if(hash1[v[x]] == undefined){
+			newArr.push(v[x])
+			hash1[v[x]] = true
+		}
+
+	console.log(newArr)
+
+  for(var i=0; i<newArr.length; i++){
+    hash[newArr[i]] = sum - newArr[i]
+  }
+
+  for(var j=0; j<newArr.length; j++){
+    for(var k=j+1; k<newArr.length; k++)
+      if(hash[newArr[j]] == newArr[k])
+        cnt++
+  }
+
+  console.log(cnt)
+}
+
+
+
+v = [2,2,20,18,4,16,6,6]
 sum = 22
 
-pairSum(v,sum)
+pair(v, sum)
